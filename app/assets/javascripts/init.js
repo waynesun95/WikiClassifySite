4b2d0cbe838e803c9edff7f68ba97607c5556e0a
Wayne Sun <waynesun95@gmail.com>
2017-04-05 00:33:27 -0400

    Added a few images, js, css files

---
+++ app/assets/javascripts/init.js
@@ -0,0 +1,15 @@
+
+
+(function($){
+  $(function(){
+
+    $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
+    $(document).ready(function(){
+      $('.scrollspy').scrollSpy();
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
